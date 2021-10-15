ab99eac200d73c35b2b27cc05a1ec316a65cc64f
mpsujek <mateusz.psujek@gmail.com>
2021-10-15 21:43:47 +0200

    Refactor of app WIP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React, { useState } from "react";
+
 import InputFields from "./components/InputFields";
 import Header from "./components/Header";
+import MealsList from "./components/MealsList";
+import ShoppingList from "./components/ShoppingList";
+
 import "./App.css";
 function App() {
+  const [mealsObject] = useState("");
   return (
     <div className="App">
       <Header />
-      <InputFields />
+      <InputFields mealsObject={mealsObject} />
+      <MealsList />
+      <ShoppingList />
     </div>
   );
 }
---
+++ src/components/MealsList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function MealsList() {
+  return (
+    <>
+      <p>Here will be diplayed Meal list with ingredients</p>
+    </>
+  );
+}
