e9708294aca0552d8f02cdf8c4b70d4a7ae72ce4
mpsujek <mateusz.psujek@gmail.com>
2022-12-10 22:45:53 +0100

    First steps, just keeping idea in one place

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-// import './App.css';
-
 function App() {
   return (
     <div className='App'>
-      <h1>Hello</h1>
+      <div className='input_form'>
+        <h3>Here Input Your meal idea and ingredients</h3>
+        <form action=''>
+          Meal name
+          <input type='text' />
+          Ingredients
+          <input type='text' />
+          Number
+          <input type='number' />
+        </form>
+      </div>
+      <div className='meals_list'>
+        <h3>If you already have meal added chose from list</h3>
+        <ul>
+          <li>Some meal name</li>
+        </ul>
+      </div>
+      <div className='shopping_list'>
+        <h3>Here you have ready shopping list</h3>
+        <ul>
+          <li>Just ingredients</li>
+        </ul>
+      </div>
     </div>
   );
 }
