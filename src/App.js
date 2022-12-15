592380f1b7997853671083c4ae8d51009b56ab91
mpsujek <mateusz.psujek@gmail.com>
2022-12-15 22:00:28 +0100

    Functionality done

--- src/App.js
+++ src/App.js
@@ -1,52 +1,124 @@
-// import { useState } from "react";
-import Input from "./UI/Input";
+import { useState } from "react";
 
 function App() {
-  // const [mealsList, setMealsList] = useState([]);
+  const [mealsList, setMealsList] = useState([]);
+  const [mealName, setMealName] = useState("");
+  const ingredientsFieldsClearState = {
+    name: "",
+    amount: "",
+  };
+  const [ingredientsFields, setIngredientsFields] = useState([
+    ingredientsFieldsClearState,
+  ]);
+
+  const addMoreIngrediets = (event) => {
+    event.preventDefault();
+    let newfield = { name: "", amount: "" };
+    setIngredientsFields([...ingredientsFields, newfield]);
+  };
+
+  const handleIngredientChange = (index, event) => {
+    let data = [...ingredientsFields];
+    data[index][event.target.name] = event.target.value;
+    setIngredientsFields(data);
+  };
+  const handleMealNameChange = (event) => {
+    event.preventDefault();
+    setMealName(event.target.value);
+  };
 
   const addMealHandler = (event) => {
     event.preventDefault();
+    const newMeal = { name: mealName, ingredients: ingredientsFields };
+    const newMealList = [...mealsList, newMeal];
+    console.log(newMealList);
+    setMealsList(newMealList);
+    setMealName("");
+    setIngredientsFields([ingredientsFieldsClearState]);
   };
 
   return (
-    <div className="App">
-      <div className="input_form">
+    <div className='App'>
+      <div className='input_form'>
         <h3>Here Input Your meal idea and ingredients</h3>
         <form onSubmit={addMealHandler}>
           <div>
-            Meal name
-            <input type="text" />
-          </div>
-          Ingredients
-          <div>
-            Name of ingredient
-            <input type="text" />
-            <Input
-              label="Amount"
-              input={{
-                id: "amount",
-                type: "number",
-                min: "1",
-                step: "0.5",
-                defaultValue: "1",
-              }}
+            <label>Meal name</label>
+            <input
+              name='meal name'
+              placeholder='Meal name'
+              type='text'
+              value={mealName}
+              onChange={handleMealNameChange}
             />
-            <button>Add more</button>
           </div>
+          {ingredientsFields.map((ingredient, index) => {
+            return (
+              <div key={index}>
+                <div>
+                  <label htmlFor={`ingredient-${index}`}>Ingredient</label>
+                  <input
+                    id={`ingredient-${index}`}
+                    value={ingredient.name}
+                    placeholder='Ingredient name'
+                    type='text'
+                    name='name'
+                    onChange={(event) => handleIngredientChange(index, event)}
+                  />
+                </div>
+                <div>
+                  <label htmlFor={`ingredient-${index}`}>Amount</label>
+                  <input
+                    name='amount'
+                    id={`ingredient-${index}`}
+                    value={ingredient.amount}
+                    type='number'
+                    step='0.5'
+                    onChange={(event) => handleIngredientChange(index, event)}
+                  />
+                </div>
+                <button onClick={addMoreIngrediets}>Add more</button>
+              </div>
+            );
+          })}
+
           <button>Add meal</button>
         </form>
       </div>
-      <div className="meals_list">
+      <div className='meals_list'>
         <h3>If you already have meal added chose from list</h3>
         <ul>
-          <li>Some meal name</li>
+          {mealsList.map((singleMeal) => {
+            return (
+              <li>
+                {singleMeal.name} (ingredients:{" "}
+                {singleMeal.ingredients.map((ingredient) => {
+                  return (
+                    <span>
+                      {ingredient.name} ({ingredient.amount})
+                    </span>
+                  );
+                })}
+                )
+              </li>
+            );
+          })}
         </ul>
       </div>
-      <div className="shopping_list">
+      <div className='shopping_list'>
         <h3>Here you have ready shopping list</h3>
         <ul>
-          <li>Just ingredients</li>
+          {mealsList.map((singleMeal) => {
+            return singleMeal.ingredients.map((ingredient) => {
+              return (
+                <li>
+                  {ingredient.name} ({ingredient.amount})
+                </li>
+              );
+            });
+          })}
         </ul>
+        <button>Copy to clipboard</button>
       </div>
     </div>
   );
--- src/UI/Input.js
+++ src/UI/Input.js
@@ -1,11 +1,19 @@
 import React from "react";
-const Input = React.forwardRef(({ label, input }, ref) => {
-  return (
-    <div>
-      <label htmlFor={input.id}>{label}</label>
-      <input ref={ref} id={input.id} {...input} />
-    </div>
-  );
-});
+const Input = React.forwardRef(
+  ({ label, input, value, onChange, index }, ref) => {
+    return (
+      <div>
+        <label htmlFor={input.id}>{label}</label>
+        <input
+          ref={ref}
+          id={input.id}
+          value={value}
+          {...input}
+          onChange={(event) => onChange(index, event)}
+        />
+      </div>
+    );
+  }
+);
 
 export default Input;
